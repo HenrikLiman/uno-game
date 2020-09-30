c6fb9caba8b730cfd7c0d13c8113562955780630
Henrik Liman <Alpner93@gmail.com>
2020-09-30 15:01:43 +0200

    added all colored number-cards

--- card.py
+++ card.py
@@ -1,6 +1,5 @@
 import pygame
 import os
-from  packege_cards import *
 
 
 class Card:
@@ -8,15 +7,19 @@ class Card:
         self.color = color
         self.val = val
         self.screen = screen
-        self.uno_card = pygame.image.load(os.path.join("packege_cards", f"ub{val}.jpg"))
+        self.uno_card = pygame.image.load(os.path.join("packege_cards", f"ub{self.val}.jpg"))
 
     def draw(self, x, y):
         # pygame.draw.rect(self.screen, self.color, (x, y, 20, 100))
         self.screen.blit(self.uno_card, (x, y))
 
     def set_cards(self):
+        if self.color == [255, 0, 0]:
+            self.uno_card = pygame.image.load(os.path.join("packege_cards", f"ur{self.val}.jpg"))
         if self.color == [0, 255, 0]:
-            self.uno_card = pygame.image.load(os.path.join("packege_cards", "ug0.jpg"))
+            self.uno_card = pygame.image.load(os.path.join("packege_cards", f"ug{self.val}.jpg"))
+        if self.color == [255, 255, 0]:
+            self.uno_card = pygame.image.load(os.path.join("packege_cards", f"uy{self.val}.jpg"))
 
     def get_x(self):
         return self.x
